309e90520f1592c570be53b0ed2e1230772fb282
Ahmed Reza <mustafareza47@gmail.com>
2023-07-13 16:14:19 +0530

    first commit

--- script.js
+++ script.js
@@ -1,34 +1,103 @@
-const videoElement = document.getElementById("video");
-const pictureInPicture = document.getElementById("button-add");
-const playButton = document.getElementById("button-play");
-const pauseButton = document.getElementById("button-pause");
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
 
-// play video
-function playVideo() {
-  videoElement.play();
-}
 
-// pause video
-function pauseVideo() {
-  videoElement.pause();
-}
-
-// for picture in picture
-async function togglePictureInPicture() {
-  try {
-    if (document.pictureInPictureElement) {
-      document.exitPictureInPicture();
-    } else if (document.pictureInPictureEnabled) {
-      video.requestPictureInPicture();
-    }
-  } catch (error) {
-    console.log("whoop, error here", error);
+// Music
+const songs = [
+  {
+    name: "Jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "Jacinto-2",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "Jacinto-3",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "Jacinto-4",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
   }
+];
+
+// check if Playing
+let isPlaying = false;
+
+// play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
 }
 
-pictureInPicture.addEventListener("click", togglePictureInPicture);
-playButton.addEventListener("click", playVideo);
-pauseButton.addEventListener("click", pauseVideo);
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
 
-// On Load
-selectMediaStream();
+// Updata DOM
+function loadSong(songs) {
+  title.textContent = songs.displayName;
+  artist.textContent = songs.artist;
+  music.src = `music/${songs.name}.mp3`;
+  image.src = `image/${songs.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+//   console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+//   console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// OnLoad
+loadSong(songs[songIndex]);
+
+// Update Progress Bar and TIme
+function updataProgressBar(e) {
+    if (isPlaying) {
+        console.log(e)
+    }
+
+}
+
+// Play or Pause Eventlistener
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+nextBtn.addEventListener("click", nextSong);
+prevBtn.addEventListener("click", prevSong);
+music.addEventListener('timeUpdate', updataProgressBar)
